58e896af877db39a572975e6b1121a162e79f86d
Rungkarn <luuca@hotmail.com>
2011-08-15 12:58:10 +0700

    native lib was creted successful

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,10 +1,16 @@
 LOCAL_PATH := $(call my-dir)
-#include $(CLEAR_VARS)
-#LOCAL_MODULE := fib
-#LOCAL_SRC_FILES := fib.c
-#nclude $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
+
+OPENCV_CAMERA_MODULES:=off
+include jni/includeOpenCV.mk
+ifeq ("$(wildcard $(OPENCV_MK_PATH))","")
+	#try to load OpenCV.mk from default install location
+	include $(TOOLCHAIN_PREBUILT_ROOT)/user/share/OpenCV/OpenCV.mk
+else
+	include $(OPENCV_MK_PATH)
+endif
+
 LOCAL_MODULE := match
 LOCAL_SRC_FILES := match.cpp
 include $(BUILD_SHARED_LIBRARY)
--- jni/Match.cpp
+++ jni/Match.cpp
@@ -2,11 +2,123 @@
 #include <assert.h>
 #include <jni.h>
 #include <string.h>
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+
+#include <opencv2\core\core.hpp>
+#include <opencv2\highgui\highgui.hpp>
+#include <opencv2\features2d\features2d.hpp>
+
+
 #include "org_example_hello_MatchLib.h"
+using namespace std;
+using namespace cv;
+
+typedef vector<KeyPoint> vecKey;
+typedef vector<DMatch> vecDMatch;
+
+const int numTopSmall = 25; // Keep only top 25 lowest distance.
+
+struct disStruct
+{
+	string strFNameOfPhoto;
+	float distance;
+};
+
+ofstream outFile;
+
+bool worseThan(const disStruct & r1,const disStruct & r2);
+void ShowResult(const disStruct & rr);
 
 JNIEXPORT jstring JNICALL Java_org_example_hello_MatchLib_matching
   (JNIEnv *env, jclass obj, jstring strFileName)
   {
+  
+  	clock_t tmStart = clock();
+	
+	string strDirFlowerDB = "flowerPicDB\\";
+	string strFNameFlowerDB = strDirFlowerDB + "files.txt";
+	string strFNameFlower;
+	string strFNameUnknownFlower = "unknownFlower.jpg";
+	ifstream inFile;
+	int count = 0;
+
+	// Extract SURF feature of unknown flower.
+	// =======================================
+	Mat imgUnknownFlower;
+	vecKey keypointOfUnknownFlower;
+	SurfFeatureDetector surf(2500.);
+	SurfDescriptorExtractor surfDesc;
+	Mat descriptorOfUnknowFlower;
+	imgUnknownFlower = imread(strFNameUnknownFlower);
+	surf.detect(imgUnknownFlower,keypointOfUnknownFlower);
+	surfDesc.compute(imgUnknownFlower,keypointOfUnknownFlower,descriptorOfUnknowFlower);
+
+	// Find number of photos.
+	// ======================
+	inFile.open(strFNameFlowerDB.c_str());
+	while(inFile >> strFNameFlower)
+	{
+		count++;
+	}
+	inFile.close();
+	inFile.clear(); // If not clear() before it will make second while command finished
+					// immediately.
+	cout << "Number of flower photo = " << count << endl << endl;
+
+	// Extract SURF feature of all photos in the DB.
+	// =============================================
+	Mat *imgFlowerDB = new Mat[count];
+	vecKey *keypointDB = new vecKey[count];
+	Mat *descriptorDB = new Mat[count];
+	BruteForceMatcher<L2<float> > matcher;
+	vecDMatch *resultOfMatchDB = new vecDMatch[count]; // Similarity between unknown and 
+													   // each in DB.
+	vector<disStruct> similarity;
+	disStruct temp;
+	count = 0;
+	inFile.open(strFNameFlowerDB.c_str());
+	while(inFile >> strFNameFlower)
+	{
+		cout << strFNameFlower << endl;
+		imgFlowerDB[count] = imread(strDirFlowerDB + strFNameFlower);
+		surf.detect(imgFlowerDB[count],keypointDB[count]);
+		surfDesc.compute(imgFlowerDB[count],keypointDB[count],descriptorDB[count]);
+
+		matcher.match(descriptorOfUnknowFlower,descriptorDB[count]
+			,resultOfMatchDB[count]);
+		nth_element(resultOfMatchDB[count].begin()
+			,resultOfMatchDB[count].begin()+numTopSmall-1
+			,resultOfMatchDB[count].end());
+		resultOfMatchDB[count].erase(resultOfMatchDB[count].begin()+numTopSmall
+			,resultOfMatchDB[count].end());
+		float sum = 0;
+		for(int i=0;i<numTopSmall;i++)
+		{
+			sum += resultOfMatchDB[count][i].distance;
+		}
+		temp.strFNameOfPhoto = strFNameFlower;
+		temp.distance = sum;
+		similarity.push_back(temp);
+		count++;
+	}
+	inFile.close();
+
+	sort(similarity.begin(),similarity.end(),worseThan);
+
+	outFile.open("Matches.txt");
+	for_each(similarity.begin(),similarity.end(),ShowResult);
+	outFile.close();
+
+	clock_t tmStop = clock();
+	cout << endl << "Total using time = " << (tmStop - tmStart)/CLOCKS_PER_SEC
+		<< " sec" << endl << "Pass enter to exit.";
+  
   	// convert Java string to UTF-8
     const char *pstr =   env->GetStringUTFChars(strFileName, NULL);
     assert(NULL != pstr);
@@ -20,6 +132,21 @@ JNIEXPORT jstring JNICALL Java_org_example_hello_MatchLib_matching
   	return env->NewStringUTF(buf);
     
   }
+  
+  
+  
+  bool worseThan(const disStruct & r1,const disStruct & r2)
+{
+	if(r1.distance < r2.distance)
+		return true;
+	else
+		return false;
+}
+
+void ShowResult(const disStruct & rr)
+{
+	outFile << rr.strFNameOfPhoto << "\t" << rr.distance << endl;
+}
 /*JNIEXPORT jlong JNICALL  Java_org_example_hello_MatchLib_test
 (JNIEnv *env, jclass obj, jint n) {
 return 1;
